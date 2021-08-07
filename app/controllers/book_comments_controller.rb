1a49b3ba2c276a2dad2837e7229d2dc8eae27bc4
yuki525 <entio777@gmail.com>
2021-08-07 12:47:38 +0000

    [Finish]応用課題6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,21 +5,23 @@ class BookCommentsController < ApplicationController
     @books = Book.new
     @book_comment = BookComment.new
 
-    book = Book.find(params[:book_id])
+
     @comment = current_user.book_comment.new(book_params)
-    @comment.book_id = book.id
+    @comment.book_id = @book.id
 
     if @comment.save
-    redirect_to book_path(book)
+
     else
 
-      render "books/show"
+      # render "books/show"
     end
   end
 
   def destroy
+    @book_comment = BookComment.new
+    @book = Book.find(params[:book_id])
     BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:book_id])
+
   end
 
   private
